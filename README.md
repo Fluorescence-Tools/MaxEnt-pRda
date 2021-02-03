148be7e44684beab61d15ba873140902dca59498
Mykola Dimura <mykola.dimura@gmail.com>
2021-02-03 23:12:42 +0100

    Switch from Normal-distributed linker p(Rda) to chi-distributed
    Additionally, faster implementation of kernel density improves
    the performance by ~two orders of magnitude.
    Update image URL in Readme. Remove old optimization results.

--- README.md
+++ README.md
@@ -4,12 +4,12 @@ Re-weighting of conformational ensembles directed by the inter-dye distance dist
 ## Usage
 
 ```
-./optimize_lif_mem.py 30000 "Lif_data/MD_Neha_pre2021/results" 0.2
+./optimize_lif_mem.py 50000 "Lif_data/MD_Neha_pre2021/results" 0.2
 ```
-First argument (`30000`) is the maximum number of optimization iterations.
+First argument (`50000`) is the maximum number of optimization iterations.
 Second argument is the path to the directory to save the results (`"Lif_data/MD_Neha_pre2021/results"`).
 Third argument (`0.2`) is `θ`. θ is the tuning parameter that allows to chose the relative weight of `χ2` and entropy (`S`). For `θ = 0` the entropy is ignored and the algorithm minimizes the `χ2`, for `θ = +∞`, `χ2` is ignored and no reweighing can happen since the original weights are fully preserved.
 This example runs MEM optimization with 30000 steps and `θ = 0.2`. The results are saved in the `Lif_data/MD_Neha_pre2021/results` directory.
 The results include the [optimized cluster weights](Lif_data/MD_Neha_pre2021/results/weights_final.dat), [optimized inter-dye distance distributions](Lif_data/MD_Neha_pre2021/results/pRda_model_137_215.dat), and corresponding plots for each FRET pair.
 
-![inter-dye distance distributions before and after MEM optimization](Lif_data/MD_Neha_pre2021/results/268_296_25308.png)
+![inter-dye distance distributions before and after MEM optimization](Lif_data/MD_Neha_pre2021/results/268_296_49800.png)
--- optimize_lif_mem.py
+++ optimize_lif_mem.py
@@ -7,35 +7,34 @@ import collections
 from functools import partial
 from glob import glob
 from multiprocessing import Pool, current_process, cpu_count
+from datetime import timedelta
+import time
+
 import numpy as np
 from numpy.random import default_rng
 from numpy.lib import recfunctions as rfn
 from scipy.stats import norm
 from scipy.interpolate import interp1d
 from scipy.special import erf
-from scipy.optimize import basinhopping
+from scipy.optimize import basinhopping, minimize
 import matplotlib.pyplot as plt
 import lmfit
 from tqdm import trange, tqdm
-
-from datetime import timedelta
-import time
 #np.seterr(all='raise')
 
-def main(nSteps, saveDirPath, theta, seed, method='BH-SLSQP'):
+def main(nSteps, saveDirPath, theta, seed, method):
     # Input parameters:
-    sigma = 6.0 #Angstrom
+    #expPathMask = 'Lif_data/ucfret_20201210/*.txt'
+    #mdDataDir = 'Lif_data/MD_Milana_Lif_ff99sb-disp'
+    expPathMask = 'Lif_data/prda_20201102_exp/*.txt'
+    mdDataDir = 'Lif_data/MD_Neha_pre2021'
+    sigma = np.sqrt(2.0*6.0**2) #Angstrom
     rda_min = 0.0
     rda_max = 200.0
-    saveStride = min(int(nSteps/10), 99) + 1
+    saveStride = min(int(nSteps/10)+1, 100)
     minErrFrac = 0.0005 # minimal allowed error for a bin
-    #expPathMask = 'Lif_data/prda_20201102_exp/*.txt'
-    mdDataDir = 'Lif_data/MD_Neha_pre2021'
-    expPathMask = 'Lif_data/ucfret_20201210/*.txt'
-    #mdDataDir = 'Lif_data/MD_Milana_Lif_ff99sb-disp'
     
     # Load MD data
-    rdaModel = np.genfromtxt(mdDataDir+'/RDAMean.dat',names=True,delimiter='\t',deletechars="")
     rmpModel = np.genfromtxt(mdDataDir+'/Rmp.dat',names=True,delimiter='\t',deletechars="")
     w0 = np.loadtxt(mdDataDir+'/cluster_weights.dat',delimiter=' ',usecols=[1]) #reference weights
     w0 /= w0.sum()
@@ -48,24 +47,26 @@ def main(nSteps, saveDirPath, theta, seed, method='BH-SLSQP'):
     else:
         wseed = np.loadtxt(seed+'/weights_final.dat',usecols=[0])
         assert wseed.shape == w0.shape
-    assert w0.shape[0] == rdaModel.shape[0]
+    assert w0.shape[0] == rmpModel.shape[0]
     
     # Format experimental data
     # Creates list of strings, which are FRET pairs, i.e. ['137_215', '137_268',...].
     # They are taken from the header of 'RDAMean.dat'
-    pairsStr = list(rdaModel.dtype.names[1:]) 
+    pairsStr = list(rmpModel.dtype.names[1:]) 
     # Appends distances of all FRET pairs into single numpy array of dimension N_clusters x Npairs
-    rdaEns = rfn.structured_to_unstructured(rdaModel[pairsStr])
+    rmpEns = rfn.structured_to_unstructured(rmpModel[pairsStr])
     # Replaces NaN values, if such exist, with -1, hence no need to clean the data from NaN occurences
     # if numpy.version.version<1.17 numpy.nan_to_num does not work -> update numpy
-    rdaEns = np.nan_to_num(rdaEns, nan=-100.0) 
+    rmpEns = np.nan_to_num(rmpEns, nan=-100.0)
     bin_edges, pExp, errExp = loadExpData(expPathMask, rda_min, rda_max, pairsStr)
-    assert np.all(errExp>=minErrFrac)
+    if np.any(errExp<minErrFrac):
+        print(f'WARNING: some experimental errors are below the threshold ({minErrFrac:.1e}), clipping!')
+    errExp = np.clip(errExp, minErrFrac, None)
     
     # Setup minimazion logging
+    rmp2pR = distHist2D(rmpEns, sigma, bin_edges)
     resiPart = partial(memResi, pExp=pExp, errExp=errExp, 
-                    rdaEns=rdaEns, sigma=sigma, 
-                    bin_edges=bin_edges, w0=w0, theta=theta)
+                    rmp2pR=rmp2pR, w0=w0, theta=theta)
     fG = lambda resi: np.square(resi).sum()
     
     numResi = resiPart(wseed).size
@@ -96,10 +97,11 @@ def main(nSteps, saveDirPath, theta, seed, method='BH-SLSQP'):
     start_time = time.monotonic()
     if method == "MH":
         minimizeMH(resiTrace, nSteps, wseed)
-    elif method[:3] == "BH-":
-        minimizeScipyBH(resiTrace, nSteps, wseed, method[3:])
+    elif method[:6] == "Lmfit_":
+        minimizeLmfit(resiTrace, nSteps, wseed, method[6:])
     else:
-        minimizeLmfit(resiTrace, nSteps, wseed, method)
+        minimizeScipy(resiTrace, nSteps, wseed, method)
+        
 
     run_duration = timedelta(seconds=int(time.monotonic() - start_time))
     tbar.close()
@@ -112,7 +114,7 @@ def main(nSteps, saveDirPath, theta, seed, method='BH-SLSQP'):
     
     # Find the best & first good solutions
     iBest = gTraj.argmin()
-    Gmin, chi2r, S, resW = weigths2GTerms(wTraj[iBest], w0, pExp, errExp, rdaEns,  sigma, bin_edges, theta)
+    Gmin, chi2r, S, resW = weigths2GTerms(wTraj[iBest], w0, pExp, errExp, rmp2pR, theta)
     iGood = np.argmax(gTraj<Gmin*1.001)
     gGood = gTraj[iGood]
     
@@ -121,6 +123,7 @@ def main(nSteps, saveDirPath, theta, seed, method='BH-SLSQP'):
             "chi2r": chi2r,
             "S": S,
             "w_tot": wTraj[iBest].sum(),
+            "theta":theta,
             "minimizer":method,
             "runtime":str(run_duration),
             "it_Gmin": iBest*saveStride,
@@ -134,7 +137,7 @@ def main(nSteps, saveDirPath, theta, seed, method='BH-SLSQP'):
     np.savetxt(saveDirPath+'/weights_final.dat', wTraj[iBest], fmt='%.6e',header=str(resDict))
     
     edges_full = np.linspace(0.0,200.0,201)
-    pModel = gaussHistogram2D(rdaEns, wTraj[iBest], sigma, edges_full)
+    pModel = distHist2D(rmpEns, sigma, edges_full)(wTraj[iBest])
     for ip, pair in enumerate(pairsStr):
         pModData = np.column_stack(((edges_full[:-1]+edges_full[1:])*0.5,pModel[:,ip]))
         np.savetxt(saveDirPath+f'/pRda_model_{pair}.dat', pModData, 
@@ -147,11 +150,11 @@ def main(nSteps, saveDirPath, theta, seed, method='BH-SLSQP'):
             continue
         gCur = gTraj[i]
         w = wTraj[i]
-        pModel = gaussHistogram2D(rdaEns, w, sigma, bin_edges)
+        pModel = rmp2pR(w)
+        pModIni = rmp2pR(w0)
         for ip, pair in enumerate(pairsStr):
-            pModIni=gaussHistogram2D(rdaEns[:,ip:ip+1], w0, sigma, bin_edges)[:,0]            
             pRmp = np.histogram(np.nan_to_num(rmpModel[pair],nan=-1.0),bins=bin_edges, weights=w)[0]
-            plot_pRda(saveDirPath+f'/{pair}_{i*saveStride:05}.png', bin_edges, pModel[:,ip], pExp[:,ip], errExp[:,ip], pModIni, pRmp)
+            plot_pRda(saveDirPath+f'/{pair}_{i*saveStride:05}.png', bin_edges, pModel[:,ip], pExp[:,ip], errExp[:,ip], pModIni[:,ip], pRmp)
        
     
     if theta>0.0:
@@ -159,8 +162,7 @@ def main(nSteps, saveDirPath, theta, seed, method='BH-SLSQP'):
 
     return resDict
 
-def residuals(weights, pExp, errExp, rdaEns, sigma, bin_edges):
-    pModel = gaussHistogram2D(rdaEns, weights, sigma, bin_edges)
+def chi2Resi(pModel, pExp, errExp):
     # Calculate optimal pair-specific model to experiment scaling factor
     # scaling factor is obtained via minimization of chi2: d(chi2)/d(modScale)=0
     # add smallest representable float to denominator to avoid division with 0
@@ -168,14 +170,15 @@ def residuals(weights, pExp, errExp, rdaEns, sigma, bin_edges):
     modScale = np.atleast_2d(modScale)
     return (pExp - pModel*modScale)/errExp # =resi of a size [Nbins,Npairs]
 
-def memResi(w, pExp, errExp, rdaEns, sigma, bin_edges, w0, theta):   
+def memResi(w, pExp, errExp, rmp2pR, w0, theta):   
     # Here 0*log(0)=0, since lim{x*log(x), x->0} = 0
     # So the smallest representable float (2.220446049250313e-16) is added
     S = np.sum(w * np.log(w / w0 + np.finfo(float).eps)) 
     # the wheights should approximately add up to 1.0
     # resw term penalizes the deviation from 1.0; 0.1 is the force constant
     resw = (w.sum() - 1.0) / 0.1 
-    resi = residuals(w, pExp, errExp, rdaEns,  sigma, bin_edges)
+    pModel = rmp2pR(w)
+    resi = chi2Resi(pModel, pExp, errExp)
     # Here sum(resi**2) ~= chi2r, not chi2, therefore normalisation
     resi *= 1.0 / np.sqrt(2.0*resi.size) #resi.size=Nbins*Npairs
     # here sqrt(S+1)*theta because later squares this whole array
@@ -186,25 +189,41 @@ def minimizeLmfit(residualsFn, nSteps, wseed, method):
     params2np = lambda pars: np.array([pars[name] for name in pars])
     resiLmfit = lambda w: residualsFn(params2np(w))
     #minimizer dictionary containing model parameters to be optimized (here weights)
-    par = lmfit.Parameters()
-    ##add all weights to minimizer dictionionary,name them as 'v1,v2,v3..'  and constrain them between 0 and 1
-    for i in range(len(wseed)):
-        par.add(f'v{i}', wseed[i], min=0.0, max=1.0)
-    res = lmfit.minimize(resiLmfit, par, method=method, max_nfev=nSteps)
+    #add all weights to minimizer dictionionary,name them as 'v1,v2,v3..'  and constrain them between 0 and 1
+    pars = lmfit.Parameters()
+    tups = [(f'v{i}', w, True, 0.0, 1.0) for i,w in enumerate(wseed) ]
+    pars.add_many(*tups)
+    
+    res = lmfit.minimize(resiLmfit, pars, method=method, max_nfev=nSteps)
     #check if the number of optimized parameters is equal to number of inital parameters
     assert len(res.params) == len(wseed)
     
     return np.array([res.params[name] for name in res.params])
 
-def minimizeScipyBH(residualsFn, nSteps, wseed, minimizer):
+def minimizeScipy(residualsFn, nSteps, wseed, minimizer, forceFakeBounds=True):
     """ Minimize using basin hopping from SciPy """
-    fG = lambda w: np.square(residualsFn(w)).sum()
-    minimizer_kwargs = {"method":minimizer, "bounds":[(0,1)]*len(wseed)}
+    bound = ['L-BFGS-B', 'Powell', 'TNC', 'trust-constr']
+    fG_base = lambda w: np.square(residualsFn(w)).sum()
+    def fG_bounds(w):
+        #The formula is taken from: https://lmfit.github.io/lmfit-py/bounds.html
+        wb = np.sqrt(np.square(w)+1.0)-1.0
+        return fG_base(wb)
+    
+    wseed_b=None
+    minimizer_kwargs = {"method":minimizer, 
+                        "options":{"maxiter":nSteps}}
+    if forceFakeBounds==False and minimizer in bound:
+        minimizer_kwargs["bounds"]=[(0,1)]*len(wseed)
+        fG=fG_base
+        wseed_b=wseed
+    else:
+        fG=fG_bounds
+        wseed_b=np.sqrt((wseed+1)**2-1)
     if minimizer=='L-BFGS-B':
-        minimizer_kwargs["options"]={"maxiter":nSteps, "maxfun":nSteps}
-
+        minimizer_kwargs["options"]["maxfun"]=nSteps
+        
     try:
-        res=basinhopping(fG, wseed, minimizer_kwargs=minimizer_kwargs)
+        res=basinhopping(fG, wseed_b, minimizer_kwargs=minimizer_kwargs)
         return np.array(res.x)
     except StopIteration as e:
         return None
@@ -238,10 +257,10 @@ def minimizeMH(residualsFn, nSteps, wseed):
             accept_ratio = sum(accBuf) / nBuf
             gBuf=gTraj[:i][-nBuf:]
             iC = int(nBuf/2)
-            Gave_old = gBuf[:iC].min()
-            Gave_new = gBuf[iC:].min()
-            kT = max((Gave_old-Gave_new)/nBuf, 1E-12)
-            if  Gave_old <= Gave_new or accept_ratio < 0.2:
+            Gmin_old = gBuf[:iC].min()
+            Gmin_new = gBuf[iC:].min()
+            kT = max((Gmin_old-Gmin_new)/nBuf, 1E-12)
+            if  Gmin_old <= Gmin_new or accept_ratio < 0.2:
                 #reduce the step size
                 nMoves = max(nMoves-1,1)
                 w_step = (1.01+w_step*3.0)/4.0
@@ -287,27 +306,41 @@ def minimizeMH(residualsFn, nSteps, wseed):
             w = w_old
             accBuf.append(0)
         
-    return wTraj[:iAcc], resiTraj[:iAcc]
-  
-def gaussHistogram2D(rdaEns, weights, sigma, bin_edges, interp=False):
-    """Calculate gaussian-smoothed p(Rda)."""
-    normCdf = lambda x: norm.cdf(x,scale=sigma)
-    if interp:
-        # Use interpolated normal distribution to improve performance ~2x
-        r =  np.linspace(-7.0*sigma, 7.0*sigma, 2001)
-        normCdf = interp1d(r, norm.cdf(r, scale=sigma), kind='linear', fill_value=(0.0,1.0), bounds_error=False, assume_sorted=True)
+    return wTraj[:iAcc], resiTraj[:iAcc]    
 
-    numBins = bin_edges.shape[0]-1
-    numPairs = rdaEns.shape[1]
-    hist2D = np.full((numBins, numPairs),0.0)
-    for ip in range(numPairs):
-        #assuming norm. distrib. here calculates x-mu, with x=bin edges from exp.data and mu=conformer/cluster distances
-        dr = np.tile(bin_edges,(rdaEns.shape[0],1)) - rdaEns[:,ip:ip+1]
-        #for dr=x-mu calculate cdf assuming normally distributed data with sigma=6
-        cdf = normCdf(dr)
-        #pdf is derivative of cdf: calculate "pdf(x)=[cdf(x2)-cdf(x1)]*cluster weight" over all bins, and then for each bin sum over all clusters
-        hist2D[:,ip] = ((cdf[:,1:]-cdf[:,:-1])*np.atleast_2d(weights).T).sum(axis=0)
-    return hist2D
+class distHist2D(object):
+    """Functor-class that calculates kernel-smoothed distributions, given the cdf of the kernel. 
+    If no kernel functions is provided, non-central chi distribution is used."""
+    def __init__(self, rmps, sigma, bin_edges, cdfFn=None):
+        numConf = rmps.shape[0]
+        numBins = bin_edges.shape[0]-1
+        numPairs = rmps.shape[1]
+        if cdfFn is None:
+            cdfFn = self.chi_cdf
+        self.p = np.empty((numPairs, numBins, numConf))
+        for pair in range(numPairs):
+            for conf in range(numConf):
+                cdf = cdfFn(bin_edges, rmps[conf,pair], sigma)
+                self.p[pair, : ,conf] = cdf[1:] - cdf[:-1]
+                
+    def chi_cdf(self, x, mu, sigma):
+        """
+        Calculates the cdf for non-central chi distribution
+        chi_cdf = integral( x/mu*(norm.pdf(x, mu, sigma) - norm.pdf(x, -mu, sigma)), x=0..x )
+        """
+        e1 = np.exp(-0.5*(mu+x)**2 / sigma**2)
+        e2 = np.exp(-0.5*(mu-x)**2 / sigma**2)
+        erf1 = erf((mu-x)/(np.sqrt(2.0)*sigma))
+        erf2 = erf((mu+x)/(np.sqrt(2.0)*sigma))
+        chi = sigma/(mu*np.sqrt(2.0*np.pi)) * (e1-e2) + 0.5*(erf2-erf1)
+        return chi
+    
+    def __call__(self, weights):
+        """Calculate kernel-smoothed p(Rda). numBins x numPairs"""
+        assert weights.shape[0] == self.p.shape[2]
+        r = np.sum(self.p*weights.reshape(1, 1, weights.shape[0]), axis=2).T
+        return r
+        #return r/r.sum(axis=0)
 
 def loadExpData(pExpPathMask, rda_min, rda_max, pairsSorted):
     # glob() allows usage of Unix matching patterns, e.g. '*.txt'. 
@@ -371,7 +404,7 @@ def plot_pRda(path, rda_edges, model, exp, err, model_initial, pRmp):
     axR.set_ylim(0.0, None)
     
     ax.set_ylim(0.0, None)
-    ax.set_xlabel('Rda / A')
+    ax.set_xlabel('Rapp / A')
     ax.set_ylabel('p(Rda) per A / A^-1')
     chi2r=np.square((model*modScale-exp)/err).mean()
     ax.set_title('FRET pair: ' + path.split('/')[-1] + f', chi2r = {chi2r:.1f}')
@@ -402,9 +435,9 @@ def plot_energies(path, resiTraj, theta, saveStride):
     fig.savefig(path,dpi=300)
     plt.close(fig)
     
-def weigths2GTerms(w, w0, pExp, errExp, rdaEns,  sigma, bin_edges, theta):
-    chi2r = np.square(residuals(w, pExp, errExp, rdaEns,  sigma, bin_edges)).mean()
-    S = np.sum(w*np.log(w/w0+1E-12))
+def weigths2GTerms(w, w0, pExp, errExp, rmp2pR, theta):
+    chi2r = np.square(chi2Resi(rmp2pR(w), pExp, errExp)).mean()
+    S = np.sum(w*np.log(w/w0+np.finfo(float).eps))
     resW = (w.sum() - 1.0) / 0.1
     G = chi2r * 0.5 + theta * (S + 1.0) + resW**2
     return G, chi2r, S, resW
@@ -448,30 +481,33 @@ if __name__ == '__main__':
     nProcesses = 1
     nSteps=int(sys.argv[1])
     saveDirPath=sys.argv[2]
-    theta=float(sys.argv[3])
+    thetaList = [float(t) for t in sys.argv[3].split(',')]
     seed = 'reference' #'random', 'reference', '<some_path>'
     if len(sys.argv) > 4:
         seed = sys.argv[4]
 
-    methods = ['slsqp', 'BH-SLSQP', 'basinhopping', 'trust-constr', 'cg', 'MH', 'BH-L-BFGS-B',
-               'ampgo',  'dual_annealing', 'least_squares', 'BH-trust-constr']
-    # differential_evolution requires several GB of RAM per run
-    #methods += ['differential_evolution']
+    methods = ['BFGS', 'L-BFGS-B', 'trust-constr',  'CG', 'Powell', 'SLSQP', 'MH']
+    # These methods work the best: 'BFGS', 'L-BFGS-B', 'trust-constr',  'CG'
+    # Powell takes more iterations to converge
+    # 'SLSQP' optimizes very well, but each iteration takes more time if the ensemble is large
+    # These require a jacobian: 'Newton-CG', 'dogleg', 'trust-ncg','trust-exact', 'trust-krylov'
+    lmfit_methods = ['slsqp', 'basinhopping', 'trust-constr', 'cg', 'least_squares']
+    methods += ["Lmfit_"+m for m in lmfit_methods]
     
     results = []
-    
     if nProcesses == 1:
-        d = main(nSteps, saveDirPath, theta, seed, methods[0])
+        d = main(nSteps, saveDirPath, thetaList[0], seed, methods[-1])
         results.append(d)
     else:
         pool = Pool(nProcesses)
         if not os.path.exists(saveDirPath): os.mkdir(saveDirPath)
         dictLst = []
         for method in methods:
-            for cur_seed in [seed, 'random', 'random']:
-                d={'nSteps':nSteps, 'theta':theta, 'seed':cur_seed, 
-                'method':method, 'saveDirPath':f'{saveDirPath}/{method}_{cur_seed}_{len(dictLst)}' }
-                dictLst.append(d)
+            for theta in thetaList:
+                for cur_seed in [seed, 'random', 'random']:
+                    d={'nSteps':nSteps, 'theta':theta, 'seed':cur_seed, 
+                    'method':method, 'saveDirPath':f'{saveDirPath}/th{theta:.3f}_{method}_{cur_seed}_{len(dictLst)}' }
+                    dictLst.append(d)
         with trange(len(dictLst)) as tbar:
             for d in pool.imap_unordered(mainWrap, dictLst):
                 results.append(d)
